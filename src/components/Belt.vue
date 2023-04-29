e398f941d0bc77a9b3491f77b0aa8dd777c711f7
othy54 <othman.bensaoula@gmail.com>
2023-04-30 01:22:42 +0200

    update

---
+++ src/components/Belt.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative w-fit-content">
+    <img class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src="/img/belt.png" alt="belt off" />
+    <img class="relative z-[2] belt-on opacity-0" src="/img/belt-on.png" alt="belt on" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { inView, animate } from "motion"
+
+onMounted(() => {
+    inView(".belt-on", (info) => {
+        const controls = animate(info.target, {opacity: 1})
+        return (_leaveInfo) => animate(info.target, {opacity: 0})
+    }, {margin: '-280px -80px -280px -80px'}) 
+})
+
+</script>
