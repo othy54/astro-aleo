87c9491052dd4c3ea7650c88119da8603fa89242
othy54 <othman.bensaoula@gmail.com>
2023-04-30 11:14:04 +0200

    update

--- src/components/Belt.vue
+++ src/components/Belt.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-fit-content">
-    <img class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src="/img/belt.png" alt="belt off" />
-    <img class="relative z-[2] belt-on opacity-0" src="/img/belt-on.png" alt="belt on" />
+    <img class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src="/img/belt.png" alt="belt off" :width="137" :height="27" loading="lazy" />
+    <img class="relative z-[2] belt-on opacity-0" src="/img/belt-on.png" alt="belt on" :width="169" :height="69" loading="lazy" />
   </div>
 </template>
 <script lang="ts" setup>
