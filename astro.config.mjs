609397120f797609ffa54f7be7cca3d5b311ee48
othy54 <othman.bensaoula@gmail.com>
2023-05-02 22:25:48 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import image from "@astrojs/image";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), image(), astroImageTools]
+  integrations: [tailwind(), vue(), image()]
 });
