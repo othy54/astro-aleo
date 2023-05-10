eb7643f445092835092a21c3af31c3c9f22c552e
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-10 10:18:32 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import vue from "@astrojs/vue";
 import image from "@astrojs/image";
 import { imagetools } from 'vite-imagetools';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), image()],
+  integrations: [tailwind(), vue(), image(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    },
+  })],
   vite: {
     plugins: [imagetools()]
   }
