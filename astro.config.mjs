278d69147c1bf15612763d117e5ecd89065ba3ea
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-10 09:23:25 +0200

    update with gtm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import image from "@astrojs/image";
-import { imagetools } from 'vite-imagetools'
+import { imagetools } from 'vite-imagetools';
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), image()],
+  integrations: [tailwind(), vue(), image(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    },
+  })],
   vite: {
     plugins: [imagetools()]
-  },
+  }
 });
